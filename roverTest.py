d7dc61bf53b12b62b860fa6f00cb6cf214a45203
quachanthony <anthony.quach@colorado.edu>
2022-10-31 09:43:30 -0700

    Core functionality working

---
+++ roverTest.py
@@ -0,0 +1,8 @@
+"""
+@desc - Write down a description for your function
+@param - Describe all input parameters the function accepts. Make sure to define the input types.
+@returns - Describe the returned output. Make sure to define the output type.
+@throws - Describe the error type the function can throw
+"""
+
+def roverTest(instructions):
